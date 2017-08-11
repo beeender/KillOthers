2c88a0ce14bfd221aabe9f1e0cb356f393b57e8a
Chen Mulong <chenmulong@gmail.com>
2017-08-11 19:24:19 +0800

    Wrap accessing to Context with try block

--- app/src/main/java/io/beeender/killothers/Killer.java
+++ app/src/main/java/io/beeender/killothers/Killer.java
@@ -15,7 +15,7 @@ public class Killer {
      *
      * @param context the {@link Context}.
      */
-    public static void tryKill(Context context) {
+    public synchronized static void tryKill(Context context) {
         int versionCode = BuildConfig.VERSION_CODE;
         String myProcessName = getMyProcessName();
         // Something is wrong with accessing the /proc .
@@ -23,18 +23,24 @@ public class Killer {
             return;
         }
 
-        SharedPreferences sharedPreferences = context.getSharedPreferences( SP_NAME_PREFIX + myProcessName,
-                MODE_PRIVATE);
+        try {
+            SharedPreferences sharedPreferences = context.getSharedPreferences( SP_NAME_PREFIX + myProcessName,
+                    MODE_PRIVATE);
 
-        if (sharedPreferences.contains(SP_VERSION_CODE) &&
-                sharedPreferences.getInt(SP_VERSION_CODE, versionCode) >= versionCode) {
-            // The current version apk has been started at least once before.
+            if (sharedPreferences.contains(SP_VERSION_CODE) &&
+                    sharedPreferences.getInt(SP_VERSION_CODE, versionCode) >= versionCode) {
+                // The current version apk has been started at least once before.
+                return;
+            }
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt(SP_VERSION_CODE, versionCode);
+            editor.apply();
+        } catch (Throwable throwable) {
+            // We have seen issues with context not being fully ready with things like getFilesDir() not working.
+            // If that happens, just return.
             return;
         }
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(SP_VERSION_CODE, versionCode);
-        editor.apply();
 
         // Then let's try to kill any other process with the same name and same uid.
         killOthers();
